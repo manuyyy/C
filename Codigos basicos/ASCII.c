d5130b08de516198dc5b9ca3fdf562ffb79bcbc0
Manu Lima <146245747+manuyyy@users.noreply.github.com>
2023-12-20 19:34:21 -0300

    Delete Codigos basicos directory

--- Codigos basicos/ASCII.c
+++
@@ -1,4 +0,0 @@
-char y = 100;
-
-printf("%d", y);   //mostra o numero da tabela ascii
-printf(" %c", y);  //mostra a letra da tabela ascii
--- Codigos basicos/antecessor_sucessor.c
+++
@@ -1,8 +0,0 @@
-    int x;
-
-    printf("Escreva x: ");
-    scanf("%d", &x);
-
-    printf("\nNumero que digitou: %d",x);
-    printf("\nSeu sucessor: %d",x+1);
-    printf("\nSeu antecessor: %d",x-1);
