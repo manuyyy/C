9e4649dd2f0a5e7cb18a0df12e451e52925c4a28
Manu Lima <146245747+manuyyy@users.noreply.github.com>
2023-12-20 01:55:53 -0300

    Create media.c

---
+++ Operadores aritméticos/media.c
@@ -0,0 +1,18 @@
+    float w, x, y, z, media;
+
+    printf("Escreva w: ");
+    scanf("%f", &w);
+
+    printf("\nEscreva x: ");
+    scanf("%f", &x);
+
+    printf("\nEscreva y: ");
+    scanf("%f", &y);
+
+    printf("\nEscreva z: ");
+    scanf("%f", &z);
+
+    media = (w + x + y + z) / 4;
+
+    printf("\nNumeros que você digitou: %.2f, %.2f, %.2f e %.2f",w,x,y,z);
+    printf("\nMédia desses números: %.2f",media);
