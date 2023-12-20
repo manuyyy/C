b5551cc4484315b7458c6aac45f5e7dcd65d370c
Manu Lima <146245747+manuyyy@users.noreply.github.com>
2023-12-20 02:25:48 -0300

    Create dado_rand.c

---
+++ Codigos basicos/dado_rand.c
@@ -0,0 +1,15 @@
+    int d1, d2, d3, d4;
+
+    srand(time(NULL));
+
+    d1 = 1 + rand() % 6;
+    d2 = 1 + rand() % 6;
+    d3 = 1 + rand() % 6;
+    d4 = 1 + rand() % 6;
+
+    printf("Lançamento de 4 dados: \n");
+
+    printf("D1: %d\n",d1);
+    printf("D2: %d\n",d2);
+    printf("D3: %d\n",d3);
+    printf("D4: %d\n",d4);
