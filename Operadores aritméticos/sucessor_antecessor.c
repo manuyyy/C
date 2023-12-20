4aaec7ce7017959bc834d0ea9bc01da2082c6e78
Manu Lima <146245747+manuyyy@users.noreply.github.com>
2023-12-20 01:55:16 -0300

    Create sucessor_antecessor.c

---
+++ Operadores aritméticos/sucessor_antecessor.c
@@ -0,0 +1,8 @@
+    int x;
+
+    printf("Escreva x: ");
+    scanf("%d", &x);
+
+    printf("\nNumero que digitou: %d",x);
+    printf("\nSeu sucessor: %d",x+1);
+    printf("\nSeu antecessor: %d",x-1);
